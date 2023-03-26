d5b4ebaeb482881d5439265111b41ca951581652
prubin22 <96958409+prubin22@users.noreply.github.com>
2023-03-25 23:08:22 -0700

    Update HelloController.php

--- HelloController.php
+++ HelloController.php
@@ -7,13 +7,13 @@ use Drupal\Core\Render\Element;
 
 class HelloController extends ControllerBase {
   public function content() {
-    // Get an instance of the NumForm.
+    // Get instance of NumForm.
     $num_form = \Drupal::formBuilder()->getForm('Drupal\forum\Form\NumForm');
 
     // Render the form.
     $num_form_output = \Drupal::service('renderer')->render($num_form);
 
-    // Output the form and the result.
+    // Output form and the result.
     return [
       '#type' => 'markup',
       '#markup' => $num_form_output,
